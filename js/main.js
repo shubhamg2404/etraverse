efc081aa041e3526dcacc52660fc3754889b90e8
shubham <shubhamg2404@gmail.com>
2019-09-08 15:59:10 +0530

    testing caoursel

--- js/main.js
+++ js/main.js
@@ -142,7 +142,7 @@ jQuery(document).ready(function($) {
 				items: 1,
 				loop: true,
 				margin: 0,
-				autoplay: false,
+				autoplay: true,
 				nav: true,
 				dots: true,
 				navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
